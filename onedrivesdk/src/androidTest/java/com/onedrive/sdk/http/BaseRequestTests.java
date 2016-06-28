734381a314fc2790540618a6b78cc5d123eaa7c9
Dabo Xu <xdb48213@gmail.com>
2016-06-28 16:27:54 -0700

    add UT

--- onedrivesdk/src/androidTest/java/com/onedrive/sdk/http/BaseRequestTests.java
+++ onedrivesdk/src/androidTest/java/com/onedrive/sdk/http/BaseRequestTests.java
@@ -1,14 +1,19 @@
 package com.onedrive.sdk.http;
 
+import android.graphics.Path;
 import android.net.Uri;
 import android.test.AndroidTestCase;
 
 import com.onedrive.sdk.core.MockClient;
 import com.onedrive.sdk.extensions.IOneDriveClient;
+import com.onedrive.sdk.options.Option;
+import com.onedrive.sdk.options.QueryOption;
 
 import junit.framework.Assert;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Test cases for (@see BaseRequest)
@@ -39,12 +44,37 @@ public class BaseRequestTests extends AndroidTestCase{
             }
         };
     }
+    
     public void testUrlEncoded() throws Exception {
         URL requestUrl = mRequest.getRequestUrl();
         final Uri.Builder expectBuilder = Uri.parse(baseUrl).buildUpon();
 
         for (String segment : testingSegments) {
             expectBuilder.appendPath(segment);
+        }
+
+        Assert.assertEquals(expectBuilder.build().toString(), requestUrl.toString());
+    }
+
+    public void testUrlWithQuery() throws Exception {
+        final String queryInUrl = "expand=foo&$select=id,name";
+        final String testUrl = baseUrl + "?" + queryInUrl;
+
+        final List<Option> options = new ArrayList<Option>();
+        final QueryOption queryInOption = new QueryOption("queryWithEncode", "!");
+        options.add(queryInOption);
+
+        mRequest = new BaseRequest(testUrl, mockClient, options, null) {
+            public IOneDriveClient getClient() {
+                return mockClient;
+            }
+        };
+
+        URL requestUrl = mRequest.getRequestUrl();
+
+        final Uri.Builder expectBuilder = Uri.parse(testUrl).buildUpon();
+        for (final Option option: options) {
+            expectBuilder.appendQueryParameter(option.getName(), option.getValue());
         }
 
         Assert.assertEquals(expectBuilder.build().toString(), requestUrl.toString());
